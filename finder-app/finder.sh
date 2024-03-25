1d5ea1c403cd0b44efae00717a777e29a38746d7
Efrain Calderon <efrain.calderon@aquarobur.com>
2024-03-25 10:42:45 +0100

    finder-app: add finder script
    Add scripts to recursively find a string in any file,
    including binaries.
    using 'find', 'grep' and 'wc'.

---
+++ finder-app/finder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "ERROR: This script requires two arguments:
+                 $0 FILES_DIR SEARCH_STR
+
+        FILES_DIR   path to a directory on the filesystem
+        SEARCH_STR  text string which will be searched within $FILES_DIR        
+"
+    exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d  "$filesdir" ]; then
+    echo "Directory $filesdir does not exist"
+    exit 1
+fi
+
+cd $filesdir
+items_count=$(find . -type f | wc -l)
+matching_lines=$(grep -r "$searchstr" | wc -l)
+echo "The number of files are $items_count and the number of matching lines are $matching_lines"
+
